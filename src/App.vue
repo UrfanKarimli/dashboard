6bd2313a0e94f3bdd76f8fb3d35d2eec964dabea
UrfanKarimli <urfankarim@gmail.com>
2024-11-28 12:07:29 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="">
+      <!-- <HelloWorld msg="You did it!" /> -->
+
+      <nav class=" flex gap-4">
+        <RouterLink class=" text-red-700"  to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+</style>
